bc6955071a82c759e71d3f573dcb52af7806a0cf
RadsammyT <radsammyt@gmail.com>
2025-09-11 16:47:59 -0400

    port: EE interaction verbs
    git apply gaming

--- Content.Shared/Verbs/VerbCategory.cs
+++ Content.Shared/Verbs/VerbCategory.cs
@@ -115,8 +115,11 @@ namespace Content.Shared.Verbs
 
         public static readonly VerbCategory Adjust =
             new("verb-categories-adjust", "/Textures/Interface/VerbIcons/screwdriver.png");
-        
+
         // Shitmed - Starlight Abductors
         public static readonly VerbCategory Switch = new("verb-categories-switch", "/Textures/Interface/VerbIcons/group.svg.192dpi.png");
+        // Einstein Engines - Interaction Verbs
+        public static readonly VerbCategory Interaction = new("verb-categories-interaction", null);
+
     }
 }
